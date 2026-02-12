9f66c69e8ef0f5db5ba291035e1a9245912b7bd9
JUAN JOSE ALZATE GARCIA <juan_alzate82212@elpoli.edu.co>
2026-02-12 11:41:27 -0500

    Favorites fixes

--- src/app/my-account/favorites/favorites.ts
+++ src/app/my-account/favorites/favorites.ts
@@ -2,16 +2,18 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FavoritesService } from './favorites.service';
 import { MoviesService } from '../../movies/movies.service';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,],
   templateUrl: './favorites.html',
   styleUrl: './favorites.css',
 })
 export class Favorites implements OnInit {
   favoritesIds: number[] = [];
+  favoriteSlots: (any | null)[] = [];
   movies: any[] = [];
   loading = true;
 
@@ -19,7 +21,7 @@ export class Favorites implements OnInit {
     private favoritesService: FavoritesService,
     private movieService: MoviesService,
     private cdr: ChangeDetectorRef,
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.loadFavorites();
@@ -27,7 +29,7 @@ export class Favorites implements OnInit {
 
   loadFavorites() {
     this.loading = true;
-    
+
     this.favoritesService.getFavorites().subscribe(ids => {
       this.favoritesIds = ids;
 
@@ -35,9 +37,19 @@ export class Favorites implements OnInit {
         ids.map(id => this.movieService.getMovieDetail(id).toPromise())
       ).then(movies => {
         this.movies = movies;
+        this.buildSlots();
         this.loading = false;
         this.cdr.detectChanges();
       });
+    });
+  }
+
+  buildSlots() {
+    this.favoriteSlots = Array(5).fill(null);
+    this.movies.forEach((movie, index) => {
+      if (index < 5) {
+        this.favoriteSlots[index] = movie;
+      }
     });
   }
 
